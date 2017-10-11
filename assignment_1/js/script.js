ef9c60070feb9cd8a32a1882df500031ff3ac7e7
Harrison Lingren <harrisonlingren@gmail.com>
2017-10-11 22:54:59 +0100

    HTML skeleton structure, sidebar menu, jquery logic and animations

--- assignment_1/js/script.js
+++ assignment_1/js/script.js
@@ -1,1 +1,70 @@
 // form functions will go here
+var menuExpanded = true;
+
+function changeMenu() {
+    if (menuExpanded) {
+        // if menu is expanded, collapse
+        $('.menu-link').hide();
+        $('nav').animate({
+            'width': '0px'
+        },
+        {
+            'duration': 200,
+            'queue': false
+        });
+
+        $('.container').animate({
+            'margin-left': '48px'
+        },
+        {
+            'duration': 200,
+            'queue': false
+        });
+        menuExpanded = false;
+
+    } else {
+        // if menu is collapsed, expand
+        $('nav').animate({
+            'width': '250px'
+        },
+        {
+            'duration': 200,
+            'queue': false
+        });
+        $('.container').animate({
+            'margin-left': '250px'
+        },
+        {
+            'duration': 200,
+            'queue': false
+        });
+        $('.menu-link').show();
+        menuExpanded = true;
+    }
+}
+
+$(document).ready(() => {
+    // show home on initial load
+    $('.menu-section').hide();
+    $('#section-home').show();
+
+    // menu click events
+    $('#menu-link-home').click(() => {
+        $('.menu-section').hide();
+        $('#section-home').show();
+    });
+
+    $('#menu-link-options').click(() => {
+        $('.menu-section').hide();
+        $('#section-options').show();
+    });
+
+    $('#menu-link-contact').click(() => {
+        $('.menu-section').hide();
+        $('#section-contact').show();
+    });
+
+    // menu expand/collapse
+    $('.menu-btn').click(changeMenu);
+
+});
---
+++ assignment_1/technical_log.md
@@ -0,0 +1,17 @@
+# Assignment 1 Technical Log
+**Harrison Lingren | COM 554**
+
+## 2017-10-11
+
+`index.html`
+- Added basic HTML skeleton structure with `div` elements to separate section content
+- Added menu section with empty links to switch between actively displayed content
+
+`script.js`
+- Added jQuery logic for menu to switch displayed content
+- Added hamburger menu animation to slide in from left
+
+`style.css`
+- Added default font family & colors
+- Added spacing for basic structural classes (`menu-section`, `header`, `footer`, etc)
+- Added menu styling to show on left of page
